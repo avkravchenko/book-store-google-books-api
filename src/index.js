0a5b7d37bb07641ef088440724bf93463ed26396
Alexander <112536439+avkravchenko@users.noreply.github.com>
2023-03-15 19:56:08 +0600

    finally i made this buttons logic

--- src/index.js
+++ src/index.js
@@ -1,125 +1,109 @@
 import { mySlider } from "./slider";
 mySlider();
 
-const selectedBooks = [];
+const cart = {};
+const checkData = localStorage.getItem('myKey');
+const parsedData = JSON.parse(checkData);
+
+
 const categories = document.querySelectorAll('.main__content__categories__list__item');
 let counter = 0;
 const numberIndi = document.querySelector('.nav__divs__image__span');
 
 
-console.log(numberIndi)
 categories.forEach(category => {
   category.style.cursor = "pointer";
   category.addEventListener('click', handleClick);
 });
 
-function handleClick(e) {
-  
+function handleClick(e){
   const clickedCategory = e.target.textContent;
     categories.forEach((item) => {
       item.classList.remove('main__content__categories__list__item--active');
     })
     this.classList.add('main__content__categories__list__item--active');
-    
-    
-    fetch(`https://www.googleapis.com/books/v1/volumes?q="subject:${clickedCategory}"&printType=books&startIndex=0&maxResults=6&langRestrict=en`)
-    .then(response => response.json())
-    .then(data => {
-      const books = data.items;
-      const pics = document.querySelectorAll('.main__content__books__card__image');
-      const auth = document.querySelectorAll('.main__content__books__card__description__authors');
-      const title = document.querySelectorAll('.main__content__books__card__description__title');
-      const desc = document.querySelectorAll('.main__content__books__card__description__text');
-      const price = document.querySelectorAll('.main__content__books__card__description__price');
-      const buttonsContainer = document.querySelectorAll('.main__content__books__card__description__button-container');
 
+   
+    function fetchingBooksData(clickedCategory) {
+      return fetch(`https://www.googleapis.com/books/v1/volumes?q="subject:${clickedCategory}"&printType=books&startIndex=0&maxResults=6&langRestrict=en`)
+      .then(response => response.json())
+      .then(data => data)
+    }
 
-      pics.forEach(pic => {
-        pic.innerHTML = '';
-      });
+    function renderBooks(book) {
+      const bookConteiner = document.querySelectorAll('.main__content__books__card');
+      const bookItems = book.items;
+      console.log(bookItems);
+      bookConteiner.forEach((conteiner, index) => {
+        const item = bookItems[index];
+        const cartData = JSON.parse(checkData);
+        // here we can try to add some staff for button rendering
+        console.log(cartData)
+        if (item) {
+          conteiner.innerHTML = `
+            <div class="main__content__books__card__image"><img src="${item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : "/images/default-cover.jpg"}"></div>
+            <div class="main__content__books__card__description">
+              <div class="main__content__books__card__description__authors">${item.volumeInfo.authors ? item.volumeInfo.authors.join(", ") : 'Authors not found'}</div>
+              <div class="main__content__books__card__description__title"><h3>${item.volumeInfo.title}</h3></div>
+              <div class="main__content__books__card__description__rating"></div>
+              <div class="main__content__books__card__description__text">${item.volumeInfo.description ? item.volumeInfo.description.slice(0, 100) + "..." : "Description not found"}</div>
+              <div class="main__content__books__card__description__price">${item.saleInfo && item.saleInfo.listPrice ? item.saleInfo.listPrice.amount + " " + item.saleInfo.listPrice.currencyCode : "Price not defined"}</div>
+              <div class="main__content__books__card__description__button-container"><button class="main__content__books__card__description__in-cart">Buy now</button></div>
+            </div>
+          `;
 
-      auth.forEach(auth => {
-        auth.innerHTML = '';
-      });
+        } else {
+          conteiner.innerHTML = `
+            <div class="main__content__books__card__image"><img src="/images/default-cover.jpg"></div>
+            <div class="main__content__books__card__description">
+              <div class="main__content__books__card__description__authors">Authors not found</div>
+              <div class="main__content__books__card__description__title"><h3>Title not found</h3></div>
+              <div class="main__content__books__card__description__rating">Rating not found</div>
+              <div class="main__content__books__card__description__text">Description not found</div>
+              <div class="main__content__books__card__description__price">Price not defined</div>
+            </div>
+          `;
+        }
 
-      title.forEach(title => {
-        title.innerHTML = '';
-      });
-
-      desc.forEach(desc => {
-        desc.innerHTML = '';
-      });
-
-      price.forEach(price => {
-        price.innerHTML = '';
-      });
-
-      buttonsContainer.forEach(button => {
-        button.innerHTML = '';
-      })
-
-
-      books.forEach((book, index) => {
-        const newImg = document.createElement("img");
-        const image = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "/images/default-cover.jpg";
-        newImg.setAttribute('src', image);
-        newImg.style.minWidth = "100%";
-        newImg.style.maxHeight = "230px";
-        pics[index].appendChild(newImg);
-
-        const newAuth = document.createElement("p");
-        const author = !book.volumeInfo.authors ? 'Authors not found' : book.volumeInfo.authors.join(", ");
-        newAuth.textContent = author;
-        auth[index].appendChild(newAuth);
-
-        const newTitle = document.createElement("h3");
-        newTitle.textContent = book.volumeInfo.title;
-        title[index].appendChild(newTitle);
-
-        const newDescription = document.createElement("p");
-        const description = !book.volumeInfo.description ? "Description not found" : book.volumeInfo.description.slice(0, 100) + "...";
-        newDescription.textContent = description;
-        desc[index].appendChild(newDescription);
         
-        const newPrice = document.createElement("h3");
-        const priccce = book.saleInfo.listPrice ? book.saleInfo.listPrice.amount + " " + book.saleInfo.listPrice.currencyCode:  "Price not defined";
-        newPrice.textContent = priccce;
-        price[index].appendChild(newPrice);
+      });
 
-        const newButton = document.createElement('button');
-        newButton.setAttribute('id', book.id);
-        newButton.innerHTML = 'By now';
-        newButton.classList.add('main__content__books__card__description__in-cart');
-        buttonsContainer[index].appendChild(newButton);
-        console.log(newButton)
-      })
+      const buttons = document.querySelectorAll('.main__content__books__card__description__in-cart');
+
       
-      const collectionButtons = document.querySelectorAll('.main__content__books__card__description__in-cart');
-      
-      console.log(selectedBooks)
-      collectionButtons.forEach(button => {
+
+      buttons.forEach((button, index) => {
+        const clickedButton = bookItems[index].id;
         button.addEventListener('click', () => {
-          const isSelected = selectedBooks.includes(button);
-          if (isSelected) {
-            counter--;
-            button.innerHTML = "By now";
-            console.log(counter);
-            selectedBooks.splice(selectedBooks.indexOf(button), 1);
-            numberIndi.innerHTML = counter;
+          if (!cart[clickedButton]) {
+            // Add to cart
+            cart[clickedButton] = true;
+            button.textContent = "in the cart";
+            counter++
+            console.log(counter)
           } else {
-            selectedBooks.push(button);
-            counter++;
-            button.innerHTML = "In the cart";
-            button.classList.add('main__content__books__card__description__in-cart--active');
-            numberIndi.innerHTML = counter;
-            numberIndi.classList.add('nav__divs__image__span--active');
+            // Remove from cart
+            delete cart[clickedButton];
+            button.textContent = "buy now";
+            counter--
+            console.log(counter)
           }
-        })
-      })
-    });
+          const saveInLocal = JSON.stringify(cart);
+          localStorage.setItem('myKey', saveInLocal);
+        });
+        // Set the initial text of the button based on the cart
+        if (cart[clickedButton]) {
+          button.textContent = "in the cart";
+        }
+      });
+    }
+
+    fetchingBooksData(clickedCategory)
+        .then(book => renderBooks(book));
 }
 
 categories[0].classList.add('main__content__categories__list__item--active');
 categories[0].click();
 
 
+   
