35769e42ac36b457e3027709bc57ff27b14971a7
Alexander <112536439+avkravchenko@users.noreply.github.com>
2023-03-26 00:31:37 +0600

    relatively save version

--- src/buttonsLogic.js
+++ src/buttonsLogic.js
@@ -3,46 +3,39 @@ import { getFromLocal } from "./getFromLocal";
 import { removeFromCart } from "./removeFromCart";
 import { removeFromLocal } from "./removeFromLocal";
 
-
-let counter = 0
+let counter = 0;
+const data = getFromLocal();
 const indi = document.querySelector('.nav__divs__image__span');
 
-
 export function handleBtnClick(place, cart) {
-    let data = getFromLocal();
   
     function handleClick(e) {
-      const clickedButton = e.target;
-      const clickedButtonId = clickedButton.getAttribute('data-id');
-  
-      if (!cart[clickedButtonId]) {
-        addToCart(clickedButtonId, cart);
-        clickedButton.textContent = "in the cart";
-        clickedButton.classList.add('main__content__books__card__description__in-cart--active');
-        console.log(cart)
-        counter++;
-        indi.innerHTML = counter;
-        indi.style.display = 'flex';
-        console.log(counter)
-      } else {
-        removeFromCart(clickedButtonId, cart);
-        removeFromLocal(clickedButtonId);
-        clickedButton.textContent = "Buy now";
-        clickedButton.classList.remove('main__content__books__card__description__in-cart--active');
-        console.log(cart)
-        counter--;
-        (counter > 0) ? indi.style.display = 'flex' : indi.style.display = 'none';
-        indi.innerHTML = counter;
-        console.log(counter)
+      if(e.target.classList.contains('main__content__books__card__description__in-cart')) {
+        const clickedButton = e.target;
+        const clickedButtonId = clickedButton.getAttribute('data-id');
+
+        if (!cart[clickedButtonId]) {
+          addToCart(clickedButtonId, cart);
+          clickedButton.textContent = "in the cart";
+          clickedButton.classList.add('main__content__books__card__description__in-cart--active');
+          console.log(cart)
+          counter++;
+          indi.innerHTML = counter;
+          indi.style.display = 'flex';
+          console.log(counter)
+        } else {
+          removeFromCart(clickedButtonId, cart);
+          removeFromLocal(clickedButtonId);
+          clickedButton.textContent = "Buy now";
+          clickedButton.classList.remove('main__content__books__card__description__in-cart--active');
+          console.log(cart)
+          counter--;
+          (counter > 0) ? indi.style.display = 'flex' : indi.style.display = 'none';
+          indi.innerHTML = counter;
+          console.log(counter)
+        }
       }
-  
-      if (data[clickedButtonId]){
-        removeFromLocal(clickedButtonId);
-        clickedButton.classList.remove('main__content__books__card__description__in-cart--active');
-      } 
-  
     }
-  
     // check if place already has an event listener
     if (place.onclick) {
       place.removeEventListener('click', place.onclick);
